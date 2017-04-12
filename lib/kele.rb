d4bc54b1d70e91826a64520ff121cdbd44df4c5d
K.Bailey <katebailey1230@gmail.com>
2017-04-12 16:29:02 -0400

    Initialize and authorize KeleClient

---
+++ lib/kele.rb
@@ -0,0 +1,14 @@
+require "httparty"
+
+class Kele
+  include HTTParty
+
+  def initialize(email, password)
+    
+    @api_url = 'https://www.bloc.io/api/v1/sessions'
+    receive = self.class.post(@api_url, body: {  'email': email, 'password': password })
+
+    raise ArgumentError, "Email or password is invalid, please try again" unless receive.code == 200
+    @auth_token = receive["auth_token"]
+  end
+end
