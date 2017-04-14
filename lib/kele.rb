d9cb1667ea7b74f3df0f818e612022235b2b0044
K.Bailey <katebailey1230@gmail.com>
2017-04-14 15:28:41 -0400

    retrieve users

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,4 +1,5 @@
 require "httparty"
+require "json"
 
 class Kele
   include HTTParty
@@ -11,4 +12,9 @@ class Kele
     raise ArgumentError, "Email or password is invalid, please try again" unless receive.code == 200
     @auth_token = receive["auth_token"]
   end
+ 
+  def get_me
+    response = self.class.get('https://www.bloc.io/api/v1/users/me', headers: { "authorization" => @auth_token })
+    JSON.parse(response.body)
+  end 
 end
